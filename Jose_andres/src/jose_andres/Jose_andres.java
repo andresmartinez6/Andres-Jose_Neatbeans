a97eca41df3f136607dc079fd57f4d040914d282
unknown <josexisco1993@gmail.com>
2023-05-09 13:11:39 +0200

    hola mundo2

--- Jose_andres/src/jose_andres/Jose_andres.java
+++ Jose_andres/src/jose_andres/Jose_andres.java
@@ -8,7 +8,7 @@ public class Jose_andres {
         Scanner teclado=new Scanner(System.in);
         
         System.out.println("Hola mundo soy Andres");
-        System.out.println("hola domun");
+        System.out.println("Hola mundo soy jose");
         
         
     }

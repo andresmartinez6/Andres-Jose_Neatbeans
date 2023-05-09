51766d32cedb980f6210f8c42a331b439c8343b4
andresmartinez6 <andresiz11x@gmail.com>
2023-05-09 13:02:27 +0200

    Te paso el hola mundo de Andrés

--- Jose_andres/src/jose_andres/Jose_andres.java
+++ Jose_andres/src/jose_andres/Jose_andres.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jose_andres;
+import java.util.Scanner;
 
-/**
- *
- * @author EAG
- */
 public class Jose_andres {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner teclado=new Scanner(System.in);
+        
+        System.out.println("Hola mundo soy Andres");
+        
+        
     }
     
 }
